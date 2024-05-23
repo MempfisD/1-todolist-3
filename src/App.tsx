28d56268c8bcf078630a19ad5a093688d0ba9f78
Dmitry Naumov <komrad.nda@yandex.ru>
2024-05-23 23:05:56 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import {TodoList} from './TodoList';
+
+// * CRUD
+// * C - create
+// * R - read
+// * U - update
+// * D - delete
+
+function App() {
+	return (
+			<div className="App">
+				<TodoList/>
+			</div>
+	);
+}
+
+export default App;
