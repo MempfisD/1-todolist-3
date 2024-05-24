8542382f5d2f6e080ac2516a59af62092e4da70f
Dmitry Naumov <komrad.nda@yandex.ru>
2024-05-24 20:15:53 +0300

    2024-05-20 - 1 урок

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import {TodoList} from './TodoList';
+import React from 'react'
+import './App.css'
+import { TaskType, TodoList } from './TodoList'
 
 // * CRUD
 // * C - create
@@ -9,11 +9,32 @@ import {TodoList} from './TodoList';
 // * D - delete
 
 function App() {
+	const todoListTitle = 'What to learn'
+	const tasks: Array<TaskType> = [
+		{ id: 1, title: 'HTML & CSS', isDone: true },
+		{ id: 2, title: 'JS & REACT', isDone: true },
+		{ id: 3, title: 'VITE', isDone: true },
+	]
+
+	const todoListTitle_1 = 'What to buy'
+	const tasks_1: Array<TaskType> = [
+		// { id: 1, title: 'Water', isDone: true },
+		// { id: 2, title: 'Beer', isDone: true },
+		// { id: 3, title: 'Meat', isDone: true },
+	]
+
 	return (
-			<div className="App">
-				<TodoList/>
-			</div>
-	);
+		<div className='App'>
+			<TodoList
+				title={todoListTitle}
+				tasks={tasks}
+			/>
+			<TodoList
+				title={todoListTitle_1}
+				tasks={tasks_1}
+			/>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React from 'react'
 
-export const TodoList = () => {
+type TodoListPropsType = {
+	title: string
+	tasks: Array<TaskType>
+}
+
+export type TaskType = {
+	id: number
+	title: string
+	isDone: boolean
+}
+
+export const TodoList = ({ title, tasks }: TodoListPropsType) => {
+	const tasksElement: Array<JSX.Element> | JSX.Element =
+		tasks.length !== 0 ? (
+			tasks.map(task => {
+				return (
+					<li>
+						<input
+							type='checkbox'
+							checked={task.isDone}
+						/>{' '}
+						<span>{task.title}</span>
+					</li>
+				)
+			})
+		) : (
+			<span>Your tasksList is empty</span>
+		)
+
 	return (
-			<div className="todoList">
-				<h3>What to learn</h3>
-				<div>
-					<input/>
-					<button>+</button>
-				</div>
-				<ul>
-					<li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-					<li><input type="checkbox" checked={true}/> <span>JS</span></li>
-					<li><input type="checkbox" checked={false}/> <span>React</span></li>
-				</ul>
-				<div>
-					<button>All</button>
-					<button>Active</button>
-					<button>Completed</button>
-				</div>
+		<div className='todoList'>
+			<h3>{title}</h3>
+			<div>
+				<input />
+				<button>+</button>
 			</div>
+			<ul>{tasksElement}</ul>
+			<div>
+				<button>All</button>
+				<button>Active</button>
+				<button>Completed</button>
+			</div>
+		</div>
 	)
 }
